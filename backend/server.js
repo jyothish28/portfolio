58b3d95d5b9da64b71f0ce3b3bee09e28bc08808
jyothish28 <157520398+jyothish28@users.noreply.github.com>
2026-04-21 15:20:49 +0530

    Update CORS origin for frontend domain

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:3000',
-      'https://your-frontend-domain.vercel.app',
+      'https://portfolio-snowy-nu-14.vercel.app/',
     ],
     methods: ['GET', 'POST'],
   })
